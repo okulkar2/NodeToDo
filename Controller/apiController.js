3a52a6dc2fe1fff92ec3ad25abe036b580614c8b
okulkar2 <omkr22@gmail.com>
2017-09-29 22:59:01 -0700

    Initial api commit1

--- Controller/apiController.js
+++ Controller/apiController.js
@@ -1,2 +1,18 @@
-apiController.js
 
+var todo = require('../Model/Todo.model');
+var bodyParser = require('body-parser');
+
+module.exports = function(app){
+
+	app.use(bodyParser.json());
+	app.use(bodyParser.urlencoded({ extended: true}));
+
+	app.get('/api/todo/:uname', function(req, res){
+
+		Todo.find()
+
+	});
+
+
+
+}
